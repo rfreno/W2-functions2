a5f65d9d00cf0143c18322dfcd91ac47aef3d329
rfreno <wintersbek@gmail.com>
2022-08-24 16:48:13 -0600

    Complete

--- arrayMethods.js
+++ arrayMethods.js
@@ -18,8 +18,8 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 // CODE HERE
-// const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
-
+const evenNumbers  = mixedNumbers.filter((num) => num % 2 === 0)
+// console.log(evenNumbers)
 
 
 ////////// PROBLEM 2 //////////
@@ -39,8 +39,8 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-// const postTaxPrices // = prices.map(/* Provide Your Callback Here );
-
+const postTaxPrices = prices.map(price => price * 1.07);
+// console.log(postTaxPrices)
 
 
 ////////// PROBLEM 3 //////////
@@ -57,8 +57,8 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-// const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
-
+const totalPopulatiom = populations.reduce((total, curr) => total + curr)
+// console.log(totalPopulatiom)
 
 
 ////////// PROBLEM 4 //////////
@@ -82,9 +82,8 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-// const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
-
-
+const myStrongest = monstersInYourPocket.filter((monster) => monster.CP > 200)
+// console.log(myStrongest)
 
 ////////// PROBLEM 5 //////////
 
@@ -99,8 +98,8 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-// CODE HERE
-
+const orderTotals = orders.map((order) => order.price + (order.price * order.tax))
+// console.log(orderTotals)
 
 
 ////////// PROBLEM 6 //////////
@@ -119,4 +118,6 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method(s) to get the sum of bobsTotal.
 */
 
-// CODE HERE
+let bobsTotal = purchases.filter((order) => order.owner === "Bob").reduce((total, curr) => total + curr.price, 0)
+
+// console.log(bobsTotal)
